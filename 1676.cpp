d5b9efa9a71b67ec3fed9eea38d656c05167b2a2
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2023-02-09 10:45:14 +0900

    Add files via upload

--- 1676.cpp
+++ 1676.cpp
@@ -13,7 +13,17 @@ int main(){
     for (int i=2;i<=N;i++){
         factorial[i] = factorial[i-1]*i;
     }
+
+    // N!을 NFac에 저장하고, 10으로 나누어 떨어지면 맨 뒤 숫자가 0이라는 뜻
+    // 10으로 나눠주고 zeroNum을 1증가 시켜준다.
+    long long NFac=factorial[N];
+    int zeroNum =0;
+    while (NFac%10==0){
+        NFac/=10;
+        zeroNum++;
+    }
+
     // N!을 출력
-    cout << factorial[N] << '\n';
+    cout << zeroNum << '\n';
     return 0;
 } 
