5d285c84c970bc71293a0954547f2216b6e4edc2
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-12-16 13:24:56 +0900

    Add files via upload

--- BOJ/1260.cpp
+++ BOJ/1260.cpp
@@ -1,73 +1,89 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
+#include <vector>
 
-#define MAX 1001
+#define MAX_N 1000
 using namespace std;
 
-int map[MAX][MAX];
-bool visited[MAX];
+vector<vector<int> > edges(MAX_N+1);
+bool visited[MAX_N+1] = {false, };
 
 // v 방문
-void DFS(int v, int N);
-void BFS(int v, int N);
+void DFS(int v);
+void BFS(int v);
 int main(){
-    // 첫 째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000),
-    // 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다.
-    int N, M, V;
-    cin >> N >> M >> V;
+    // [input]
+    // 첫 째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000). 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다.
+    int n, m, v;
+    cin >> n >> m >> v;
     // 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다.
-    // 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
-    for (int i=0; i<M; i++){
+    // 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 
+    for (int i = 0; i < m; i++)
+    {
         int a, b;
         cin >> a >> b;
-        map[a][b] = 1;
-        map[b][a] = 1;
+        // 입력으로 주어지는 간선은 양방향이다.
+        edges[a].push_back(b);
+        edges[b].push_back(a);
     }
+    
+    // [solution]
+    // 방문할 수 있는 정점이 여러 개인 경우 정점 번호가 작은 것을 먼저 방문
+    for (int i = 1; i <= n; i++)
+    {
+        sort(edges[i].begin(), edges[i].end());
+    }
+    
     // DFS
     // visited 배열 0으로 초기화
-    fill_n(visited, N+1, 0);
-    DFS(V, N);
+    fill_n(visited, n+1, 0);
+    DFS(v);
 
     cout << '\n';
     
     // BFS
     // visited 배열 0으로 초기화
-    fill_n(visited, N+1, 0);
-    BFS(V, N);
+    fill_n(visited, n+1, 0);
+    BFS(v);
 
     return 0;
 }
 
-// DFS는 stack을 이용하거나 재귀적으로 구현할 수 있음.
-void DFS(int v, int N){
-    visited[v] =1; // 방문했음 체크
+void DFS(int v){
+    // 방문한 곳은 다시 탐색하지 않음
+    if(visited[v]) return;
+    visited[v]=true;
     cout << v << " ";
-    // N보다 같거나 작을때까지 반복해야 마지막 N번째 숫자까지 출력된다.
-    for (int i=1; i<=N; i++){
-        // 현재 정점과 이어져 있으면서 방문하지 않은 노드
-        if(map[v][i]==1 && visited[i]==0){
-            DFS(i, N);
-        }
-    }
 
+    // v에 연결된 노드 탐색
+    for (int i = 0; i < edges[v].size(); i++)
+    {
+        DFS(edges[v][i]);
+    }
+    
 }
 
-// BFS는 queue를 사용해서 구현하는 것이 일반적인 것 같다.
-void BFS(int v, int N){
+void BFS(int v){
     queue<int> q;
+    
     q.push(v);
-    visited[v] =1; // 방문했음 체크
+    visited[v]=true;
+
     while(!q.empty()){
-        v=q.front();
+        int node = q.front();
         q.pop();
-        cout << v << " "; // 방문했으니까 출력
-        // N보다 같거나 작을때까지 반복해야 마지막 N번째 숫자까지 출력된다.
-        for (int i=1; i<=N; i++){
-            // 현재 정점과 이어져 있으면서 방문하지 않은 노드
-            if(map[v][i]==1 && visited[i]==0){
-                q.push(i);
-                visited[i] = 1;
-            }
+        cout << node << " ";
+
+        for (size_t i = 0; i < edges[node].size(); i++)
+        {
+            int connectedNode = edges[node][i];
+            // 방문했으면 건너뛰기
+            if(visited[connectedNode]) continue;
+
+            q.push(connectedNode);
+            visited[connectedNode]=true;
         }
+        
     }
 }
